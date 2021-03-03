64b2caeecc2f36e8c42159777fe56febff3aa0f6
martacamacho <martacamachovargas@gmail.com>
2021-03-04 00:15:32 +0100

    questions in progress

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,15 @@
     <div>
         <b-nav tabs>
             <b-nav-item disabled><b>Fancy Quizz App</b></b-nav-item>
-            <b-nav-item>Counter 4/10</b-nav-item>
+            <b-nav-item>Counter {{ numCorrect }}/{{ numTotal }}</b-nav-item>
         </b-nav>
     </div>
 </template>
+<script>
+export default {
+    props: [
+        'numCorrect',
+        'numTotal'
+    ]
+}
+</script>
